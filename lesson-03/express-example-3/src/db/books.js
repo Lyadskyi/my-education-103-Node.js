2cd951bca24b3b99b499b7785e85f458bd7175c6
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-09-01 18:50:21 +0300

    Update lesson-03\express-example-3

---
+++ lesson-03/express-example-3/src/db/books.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: "u9nodNWGi3uUUwh0f8V49",
+    title: "В пошуках Аляски",
+    author: "Джон Грін",
+  },
+  {
+    id: "YnuM4QDxPeA3SmPHqEZPJ",
+    title: "Історія Риму",
+    author: "Теодор Моммзен",
+  },
+  {
+    id: "zCd_RoeNMOBbQwAXpy8Px",
+    title: "Останнє полювання",
+    author: "Жан-Крістоф Гранже",
+  },
+];
