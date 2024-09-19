950f7c1fd5662eaa26843091cd098491a852a12a
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-09-19 21:50:47 +0300

    Created the ninth lesson

---
+++ lesson-09/project-backend/src/routers/movies.js
@@ -0,0 +1,47 @@
+import { Router } from "express";
+
+import {
+  getAllMoviesController,
+  getMovieByIdController,
+  addMovieController,
+  upsertMovieController,
+  patchMovieController,
+  deleteMovieController,
+} from "../controllers/movies.js";
+
+import isValidId from "../middlewares/isValidId.js";
+
+import ctrlWrapper from "../utils/ctrlWpapper.js";
+import validateBody from "../utils/validatebody.js";
+
+import { movieAddSchema, moviePatchSchema } from "../validation/movies.js";
+
+const moviesRouter = Router();
+
+moviesRouter.get("/", ctrlWrapper(getAllMoviesController));
+
+moviesRouter.get("/:id", isValidId, ctrlWrapper(getMovieByIdController));
+
+moviesRouter.post(
+  "/",
+  validateBody(movieAddSchema),
+  ctrlWrapper(addMovieController),
+);
+
+moviesRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(movieAddSchema),
+  ctrlWrapper(upsertMovieController),
+);
+
+moviesRouter.patch(
+  "/:id",
+  isValidId,
+  validateBody(moviePatchSchema),
+  ctrlWrapper(patchMovieController),
+);
+
+moviesRouter.delete("/:id", isValidId, ctrlWrapper(deleteMovieController));
+
+export default moviesRouter;
