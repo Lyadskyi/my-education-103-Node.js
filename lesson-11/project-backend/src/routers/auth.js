44f14a590a49695929274117611a3455c3dc112e
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-10-02 22:15:48 +0300

    Added the eleventh lesson to the project
    Added a folder: lesson-11\slides

---
+++ lesson-11/project-backend/src/routers/auth.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+
+import * as authControllers from "../controllers/auth.js";
+
+import validateBody from "../utils/validatebody.js";
+import ctrlWrapper from "../utils/ctrlWpapper.js";
+
+import { userSignupSchema, userSigninSchema } from "../validation/users.js";
+
+const authRouter = Router();
+
+authRouter.post(
+  "/signup",
+  validateBody(userSignupSchema),
+  ctrlWrapper(authControllers.signupController),
+);
+
+authRouter.post(
+  "/signin",
+  validateBody(userSigninSchema),
+  ctrlWrapper(authControllers.signinController),
+);
+
+authRouter.post("/refresh", ctrlWrapper(authControllers.refreshController));
+
+authRouter.post("/signout", ctrlWrapper(authControllers.signoutController));
+
+export default authRouter;
